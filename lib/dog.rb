10cae6b8c25434ac99c67244ed3c2b7ed8facc6b
Hussain Ahmed <haz6798@icloud.com>
2020-06-07 03:09:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,3 @@
-class Dog
-  
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
-end
 
 class Dog 
   def initialize(name, breed = "Mutt")
